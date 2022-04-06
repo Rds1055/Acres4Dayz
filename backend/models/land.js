affe1e3239c5825e73196a0d403a5da668138c3e
Rds1055 <rdschaefer@mail.smu.edu>
2022-04-06 13:32:57 -0500

    User and land models

--- backend/models/land.js
+++ backend/models/land.js
@@ -8,21 +8,32 @@ class land {
         this.disconnect();
     }
 
-    async fetchAllSpaces() {
-        const results = await this.DBQuery("SELECT * FROM Bid");
+    async getAllLand() {
+        const results = await this.DBQuery("SELECT * FROM Land");
         return results;
     }
 
     async updateLandDescription(id, description) {
-        
+        const results = await this.DBQuery("UPDATE Land SET description = ? WHERE ID = ?", [description, id]);
+        const newRecord = await this.DBQuery("SELECT * FROM Land WHERE ID = ?", [id]);
+        return newRecord;
     }
 
     async updateIsAvailable(id, available) {
-
+        const results = await this.DBQuery("UPDATE Land SET is_available = ? WHERE ID = ?", [available, id]);
+        const newRecord = await this.DBQuery("SELECT * FROM Land WHERE ID = ?", [id]);
+        return newRecord;
     }
 
     async updateImage(id, image) {
+        const results = await this.DBQuery("UPDATE Land SET image = ? WHERE ID = ?", [image, id]);
+        const newRecord = await this.DBQuery("SELECT * FROM Land WHERE ID = ?", [id]);
+        return newRecord;
+    }
 
+    async getLandById(id) {
+        const results = await this.DBQuery("SELECT * FROM Land WHERE ID = ?", [id]);
+        return results;
     }
 
     async getLandByLocation(lat, long) {
@@ -30,11 +41,13 @@ class land {
     }
 
     async getLandByAcres(acres) {
-
+        const results = await this.DBQuery("SELECT * FROM Land WHERE Acres > ?", [acres]);
+        return results;
     }
 
     async getLandByOwner(owner) {
-
+        const results = await this.DBQuery("SELECT * FROM Land WHERE owner = ?", [owner]);
+        return results;
     }
 
     async getLandBySuitability(suitable) {
--- backend/models/user.js
+++ backend/models/user.js
@@ -3,46 +3,70 @@ const bcrypt = require("bcrypt");
 
 const USER_TABLE = "User";
 
-const createNewUser = async (username, password) => {
-    const hashedPassword = bcrypt.hashSync(password, 10);
-
-    const query = knex(USER_TABLE).insert({username, password: hashedPassword});
-    const result = await query;
-
-    return result;
-};
-
-const findUserByUsername = async (username) => {
-    const query = knex(USER_TABLE).where({username});
-    const result = await query;
-    return result;
-};
-
-const authenticateUser = async (username, password) => {
-    const users = await findUserByUsername(username);
-    if (users.length === 0) {
-        console.error(`No users matched the username ${username}`);
-        return false;
+class user {
+    constructor(_DBQuery, _disconnect) {
+        this.DBQuery = _DBQuery;
+        this.disconnect = _disconnect;
     }
-    const user = users[0];
-    const validPassword = await bcrypt.compare(password, user.password);
-    return validPassword;
-};
 
-const updateUserEmail = async (username, email) => {
-    //const results = knex(USER_TABLE).
-};
+    close () {
+        this.disconnect();
+    }
 
-const updateUserPhone = async (username, phone) => {
+    async createNewUser(username, password) {
+        const hashedPassword = bcrypt.hashSync(password, 10);
+        const results = await this.DBQuery("INSERT INTO User(username, password) VALUES (?, ?)", [username, hashedPassword]);
+        const newRecord = await this.DBQuery("SELECT * FROM User WHERE username = ?", [username]);
+        // const query = knex(USER_TABLE).insert({username, password: hashedPassword});
+        // const result = await query;
 
-};
+        return newRecord;
+    };
 
-const updateUserImage = async (username, image) => {
+    async findUserByUsername(username) {
+        // const query = knex(USER_TABLE).where({username});
+        // const result = await query;
+        // return result;
+        const results = await history.DBQuery("SELECT * FROM User WHERE username = ?", [username]);
+        return results;
+    };
 
-};
+    async authenticateUser(username, password) {
+        const users = await findUserByUsername(username);
+        if (users.length === 0) {
+            console.error(`No users matched the username ${username}`);
+            return false;
+        }
+        const user = users[0];
+        const validPassword = await bcrypt.compare(password, user.password);
+        return validPassword;
+    };
 
-module.exports = {
-    createNewUser,
-    findUserByUsername,
-    authenticateUser
-};
+    async updateUserEmail(username, email) {
+        const results = await this.DBQuery("UPDATE User SET email = ? WHERE username = ?", [email, username]);
+        const newRecord = await this.DBQuery("SELECT * FROM User WHERE username = ?", [username]);
+        return newRecord;
+    };
+
+    async updateUserPhone(username, phone) {
+        const results = await this.DBQuery("UPDATE User SET phone = ? WHERE username = ?", [phone, username]);
+        const newRecord = await this.DBQuery("SELECT * FROM User WHERE username = ?", [username]);
+        return newRecord;
+    };
+
+    async updateUserImage(username, image) {
+        const results = await this.DBQuery("UPDATE User SET image = ? WHERE username = ?", [image, username]);
+        const newRecord = await this.DBQuery("SELECT * FROM User WHERE username = ?", [username]);
+        return newRecord;
+    };
+}
+
+// module.exports = {
+//     createNewUser,
+//     findUserByUsername,
+//     authenticateUser,
+//     updateUserEmail,
+//     updateUserPhone,
+//     updateUserImage
+// };
+module.exports = user;
